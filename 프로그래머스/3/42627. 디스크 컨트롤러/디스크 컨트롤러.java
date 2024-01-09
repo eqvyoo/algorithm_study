e0bbaeaf52dd2ce3b7bfd5afbcb2549c9e10f0c4
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-01-09 22:07:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.51 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,17 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
+        
         Arrays.sort(jobs, (a,b) -> a[0]-b[0]);
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
+        
         int cnt = 0;
         int total = 0;
         int idx = 0;
         int end = 0;
+        
         while (cnt < jobs.length){
             while(idx < jobs.length && jobs[idx][0] <= end){
                 pq.add(jobs[idx++]);
             }
-            if (pq.isEmpty()){
+            if (pq.isEmpty()) {
                 end = jobs[idx][0];
             }else{
                 int[] curr = pq.poll();
@@ -20,9 +23,7 @@ class Solution {
                 end += curr[1];
                 cnt++;
             }
-            
         }
-        
         return total/jobs.length;
     }
 }
