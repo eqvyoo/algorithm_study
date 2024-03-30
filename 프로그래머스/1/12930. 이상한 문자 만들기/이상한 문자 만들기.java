303cacf460729df1458b0c199f24a0edf2e8e1d1
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-03-30 20:04:16 +0000

    [level 1] Title: 이상한 문자 만들기, Time: 10.78 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int idx = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == ' '){
+                answer+= s.charAt(i);
+                idx = 0;
+            }
+            else if (idx % 2 == 0){
+                answer+= Character.toUpperCase(s.charAt(i));
+                idx ++;
+            }else if(idx % 2 == 1){
+                answer += Character.toLowerCase(s.charAt(i));
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
