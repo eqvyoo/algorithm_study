41bd21c5a1408f5c2ad00a93e18b25b1dc18422f
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-05-22 18:18:31 +0900

    [level 2] Title: 기능개발, Time: 2.25 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,31 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+    public int[] solution(int[] p, int[] s) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for (int i = 0; i < p.length; i++){
+            int day = (100-p[i]) / s[i];
+            if ((100-p[i]) % s[i] != 0){
+                day ++;
+            }
+            queue.offer(day);
+            System.out.println(day);
+        }
+        
+        while (!queue.isEmpty()){
+            int cnt = 1;
+            int deploy = queue.poll();
+            while((!queue.isEmpty())&&(queue.peek() <= deploy)){
+                cnt++;
+                queue.poll();
+            }
+            answer.add(cnt);
+        }
+        return answer.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
