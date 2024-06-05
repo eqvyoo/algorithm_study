07dc1ef291cb7117420d71e726433f82b17fc09d
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-06-05 22:29:15 +0900

    [level 2] Title: 구명보트, Time: 9.20 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int idx = 0;
+        for(int i = people.length -1; i >= idx; i--){
+            if (people[idx] + people[i] <= limit){
+                answer++;
+                idx++;
+            }
+            else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
