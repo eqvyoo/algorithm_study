a557f434053a69afd6e026306153702ce5246194
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-07-28 20:55:48 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public void dfs(int node, boolean[] visited, int[][] computers){
+        visited[node] = true;
+        
+        for (int i = 0; i < computers.length; i++){
+            if (visited[i] == false && computers[node][i] == 1)
+                dfs(i,visited, computers);
+        }
+    }
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        int len = computers.length;
+        boolean[] visited = new boolean[len];
+        
+        for(int i = 0; i < len; i++){
+            visited[i] = false;
+        }
+        
+        for (int i = 0; i < len; i++){
+            if(visited[i] == false){
+                answer++;
+                dfs(i,visited, computers);
+            }
+        }
+        return answer;
+    }
+}
